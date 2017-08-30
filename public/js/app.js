af87bdb1b1874a49a3ddd3696ea166a063e8d0f4
Cathleen Wright <cathleenmwright@gmail.com>
2017-08-30 16:35:21 -0400

    Successful post route via angular

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,20 @@
 const app = angular.module('QuotesApp', []);
 
-app.controller('MainController', function(){
-  this.foo = 'bar';
-});
+app.controller('MainController', ['$http', function($http){
+
+  this.createQuote = function() {
+    $http({
+      method: 'POST',
+      url: '/quotes',
+      data: {
+        quote: this.quote,
+        character: this.character
+      }
+    }).then(function(response){
+      console.log(response);
+    }, function() {
+      console.log('error');
+    });
+  }
+
+}]); //end of MainController
