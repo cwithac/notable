a0334759d1ee5e8bce7b103fb57677a13d3804c8
Cathleen Wright <cathleenmwright@gmail.com>
2017-08-30 13:23:47 -0400

    Update Route

--- controllers/quotes.js
+++ controllers/quotes.js
@@ -20,4 +20,10 @@ router.delete('/:id', (req, res) => {
   });
 });
 
+router.put('/:id', (req, res) => {
+  Quotes.findByIdAndUpdate(req.params.id, req.body, {new:true}, (err, updatedTodo) => {
+    res.json(updatedTodo)
+  });
+});
+
 module.exports = router;
