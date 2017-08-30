24d5836222cd158e87da2add636d3808241111b6
Cathleen Wright <cathleenmwright@gmail.com>
2017-08-30 12:34:56 -0400

    Initialize mongoose

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+const quotesController = require('./controllers/quotes.js');
+app.use('/quotes', quotesController);
 
 app.listen(3000, () => {
   console.log('quotable app is listening');
 });
+
+mongoose.connect('mongodb://localhost:27017/quote');
+mongoose.connection.once('open', () => {
+  console.log('quotable app is connected to mongo');
+})
