f960bf4fa04cfc2d8a7ad1f1210b1800dbb7481e
Cathleen Wright <cathleenmwright@gmail.com>
2017-08-30 16:47:09 -0400

    Post and get via angular

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,20 @@ const app = angular.module('QuotesApp', []);
 
 app.controller('MainController', ['$http', function($http){
 
+  const controller = this;
+
+  this.getQuotes = function() {
+    $http({
+      method: 'GET',
+      url: '/quotes',
+    }).then(function(response){
+      console.log(response);
+      controller.quotes = response.data;
+    }, function() {
+      console.log('error');
+    });
+  };
+
   this.createQuote = function() {
     $http({
       method: 'POST',
@@ -12,9 +26,12 @@ app.controller('MainController', ['$http', function($http){
       }
     }).then(function(response){
       console.log(response);
+      controller.getQuotes();
     }, function() {
       console.log('error');
     });
-  }
+  };
+
+this.getQuotes(); //page load
 
 }]); //end of MainController
