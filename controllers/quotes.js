fe6fa6c0bc9495fc5e997d773df8dc8de8c23cb4
Cathleen Wright <cathleenmwright@gmail.com>
2017-08-30 12:43:55 -0400

    Post route tested via curl

--- controllers/quotes.js
+++ controllers/quotes.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const Quotes = require('../models/quotes.js');
 
 router.get('/', (req, res) => {
   res.send('index');
+});
+
+router.post('/', (req, res) => {
+  Quotes.create(req.body, (err, createdQuote) => {
+    res.json(createdQuote);
+  });
 });
 
 module.exports = router;
