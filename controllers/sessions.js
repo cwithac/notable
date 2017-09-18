1826d6657fbc748a0948c378033fe6a0229c3c22
Cathleen Wright <cathleenmwright@gmail.com>
2017-09-18 18:57:40 -0400

    Sessions with bcrypt setup

---
+++ controllers/sessions.js
@@ -0,0 +1,8 @@
+const express   = require('express');
+const router    = express.Router();
+const User      = require('../models/users.js');
+const bcrypt    = require('bcrypt');
+
+
+
+module.exports = router;
